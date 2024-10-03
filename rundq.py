43aeb1b5863558b4e06ab9bc0197f5452a82fe1f
Techie <149437995+FarRza111@users.noreply.github.com>
2024-10-03 20:04:43 +0300

    Create rundq.py

---
+++ rundq.py
@@ -0,0 +1,59 @@
+import os
+import logging
+import datetime
+import pandas as pd
+import pyodbc as odbc
+
+
+# reading master queries from master_metrics.sql
+def read_sql_file(file_name):
+    with open(file_name, 'r') as file:
+        return file.read()
+
+
+path = os.getcwd()
+master_query = os.path.join(path, "master_metrics.sql")
+master_metrics = read_sql_file(master_query)
+connection = odbc.connect('DSN=impala', autocommit=True)
+data = pd.read_sql(master_metrics, connection)
+
+logging.getLogger().setLevel(logging.INFO)
+
+try:
+    conn = odbc.connect('DSN=impala', autocommit=True)
+    cursor = conn.cursor()
+    today = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+    cursor.execute("SELECT COUNT(*) FROM sales_db.master_metrics WHERE run_date = ?", (today,))
+    existing_records = cursor.fetchone()[0]
+
+    if existing_records > 0:  # used to check if already exists data there
+        print(f"Today's data already exists in the table. No new records inserted on {datetime.datetime.now()}")
+    else:
+        column_names = ", ".join(data.columns)
+        value_placeholder = ", ".join(["?"] * len(data.columns))
+
+        insert_query = f"INSERT INTO sales_db.master_metrics ({column_names}) VALUES ({value_placeholder})"
+
+        values = [tuple(row.values) for _, row in data.iterrows()]  # data should be fetch first
+
+        # print(insert_query)  # For debugging
+        # print(values)  # For debugging
+        cursor.executemany(insert_query, values)
+
+        # Commit changes
+        conn.commit()
+
+        message = f"Rule table updated with {len(values)} records on {datetime.datetime.now()}"
+        logging.info(message)
+        print(message)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    # Connection must be closed in the end
+    if conn:
+        conn.close()
+
+
+
